7c803b0bb16f86d2ffbb53fb7085c6585e2a82a4
Dharanidhar Puranam <27700031+Dharanidharp@users.noreply.github.com>
2022-01-13 17:31:40 +0530

    merge soundmanager

---
+++ Assets/SoundManager/SoundManager.cs
@@ -0,0 +1,53 @@
+using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class SoundManager : MonoBehaviour
+{
+    public Sounds[] sounds;
+
+    public static SoundManager instance;
+
+    AudioSource source;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sounds s in sounds) 
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.audioClip;
+            s.source.volume = s.volume;
+            s.source.loop = s.loop;
+        }
+    }
+
+    private void Start()
+    {
+        PlaySound("background_music");
+    }
+
+    public void PlaySound(string name)
+    {
+        Sounds s = Array.Find(sounds, sound => sound.name == name);
+
+        if (s == null)
+        {
+            Debug.LogWarning("Sound with " + name + " cannot be found");
+            return;
+        }
+
+        s.source.Play();
+        
+    }
+}
