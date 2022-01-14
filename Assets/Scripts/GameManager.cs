a13118f6d66737165799ad63e768c2d6bf678700
Dharanidhar Puranam <27700031+Dharanidharp@users.noreply.github.com>
2022-01-14 10:29:12 +0530

    Scene management

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -22,12 +22,24 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        LoadLevel("Main");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        Debug.Log("Loading " + levelName);
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void QuitGame() 
+    {
+        Debug.Log("Quit");
+        Application.Quit();
     }
 }
